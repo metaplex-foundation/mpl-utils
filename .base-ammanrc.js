a0772fd627a5b6db67adcc5e951023045486d44e
Sarah Strange <strange1120@gmail.com>
2025-04-13 16:08:05 -0400

    refactor: remove any files that were javascript/typescript related

--- .base-ammanrc.js
+++ .base-ammanrc.js
@@ -9,38 +9,8 @@ function localDeployPath(programName) {
   return path.join(localDeployDir, `${programName}.so`);
 }
 
-const programs = {
-  metadata: {
-    label: 'Metadata',
-    programId: 'metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s',
-    deployPath: localDeployPath('mpl_token_metadata'),
-  },
-  token_sale: {
-    label: 'Fixed Price Token Sale',
-    programId: 'SaLeTjyUa5wXHnGuewUSyJ5JWZaHwz3TxqUntCE9czo',
-    deployPath: localDeployPath('mpl_fixed_price_sale'),
-  },
-  candy_machine: {
-    label: 'Candy Machine',
-    programId: 'cndy3Z4yapfJBmL3ShUp5exZKqR3z33thTzeNMm2gRZ',
-    deployPath: localDeployPath('mpl_candy_machine'),
-  },
-  hydra: {
-    label: 'Hydra',
-    programId: 'hyDQ4Nz1eYyegS6JfenyKwKzYxRsCWCriYSAjtzP4Vg',
-    deployPath: localDeployPath('mpl_hydra'),
-  },
-  candy_machine_core: {
-    label: 'Candy Machine Core',
-    programId: 'CndyV3LdqHUfDLmE5naZjVN8rBZz4tqhdefbAnjHG3JR',
-    deployPath: localDeployPath('mpl_candy_machine_core'),
-  },
-  token_auth_rules: {
-    label: 'Token Auth Rules',
-    programId: 'auth9SigNpDKz4sJJ1DfCTuZrZNSAgh9sFD3rboVmgg',
-    deployPath: localDeployPath('mpl_token_auth_rules'),
-  },
-};
+// Only keeping core configuration without specific programs
+const programs = {};
 
 const validator = {
   killRunningValidators: true,
